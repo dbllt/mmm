779341efaa865e3e04981e27a46f4a965297f242
Dorian Bouillet <dorian.bouillet@gmail.com>
2020-11-09 13:49:27 +0100

    TP1 1.1

---
+++ app/src/main/java/mmm/tp1/MainActivity.java
@@ -0,0 +1,85 @@
+package mmm.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import mmm.tp1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.clear:
+                menuItemClear();
+                return true;
+            case R.id.telephone:
+                menuItemTelephone();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public void onClickValider(View view) {
+        String s = "Nom : " + binding.nom.getText() + "\nPrénom : " + binding.prenom.getText() +
+                "\nDate de naissance : " + binding.dateNaissance.getText() +
+                "\nVille de naissance : " + binding.villeNaissance.getText();
+        Toast.makeText(getApplicationContext(),
+                s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void menuItemClear() {
+        binding.nom.setText("");
+        binding.prenom.setText("");
+        binding.dateNaissance.setText("");
+        binding.villeNaissance.setText("");
+    }
+
+
+    public void menuItemTelephone() {
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.myLayout);
+        EditText telephone = new EditText(this);
+        telephone.setHint("Téléphone");
+        telephone.setInputType(InputType.TYPE_CLASS_PHONE);
+
+        layout.addView(telephone);
+
+
+    }
+}
